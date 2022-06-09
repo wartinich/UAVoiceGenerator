93329f8f5db75c432e9bb6e2038fdcdec230789d
wartinich <vasilenko.arhip@gmail.com>
2022-06-09 16:23:11 +0300

    Customize User

--- src/config/settings.py
+++ src/config/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """Customize User"""
+    phone_number = models.CharField(verbose_name="Phone numer", max_lenght=13)
