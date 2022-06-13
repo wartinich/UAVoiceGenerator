35b1c18999360e7b1bb70e4696e4bb3d889acbe5
wartinich <vasilenko.arhip@gmail.com>
2022-06-13 12:53:28 +0300

    Implement logout

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from users.views import SignUpPageView, LoginPageView
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
-    path('sign_up', SignUpPageView.as_view(), name='sign_up'),
-    path('login', LoginPageView.as_view(), name='login')
+    path('sign_up/', SignUpPageView.as_view(), name='sign_up'),
+    path('login/', LoginPageView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(template_name='auth/login.html'), name='logout'),
 ]
