95b3d80e22443b28ed2a9097529a07d5c88d9053
wartinich <vasilenko.arhip@gmail.com>
2022-06-13 10:38:01 +0300

    Customize User model

--- src/users/models.py
+++ src/users/models.py
@@ -4,4 +4,16 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     """Customize User"""
-    phone_number = models.CharField(verbose_name='Phone number', max_length=13)
+    SEX = [
+        ('male', 'Male'),
+        ('female', 'Female')
+    ]
+
+    first_name = models.CharField(verbose_name='First name', max_length=25)
+    last_name = models.CharField(verbose_name='Last name', max_length=40)
+    email = models.EmailField(verbose_name='Email', max_length=150, unique=True)
+    avatar_image = models.ImageField(verbose_name='Avatar', upload_to='avatars/')
+    birth_date = models.DateField(verbose_name='Birth date')
+    sex = models.CharField(verbose_name='Sex', choices=SEX, max_length=7)
+
+    REQUIRED_FIELDS = ['email',]
