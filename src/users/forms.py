2ed6026a1ec421cd81c965e33622877776678ebd
wartinich <vasilenko.arhip@gmail.com>
2022-06-14 11:19:47 +0300

    Styling register form

--- src/users/forms.py
+++ src/users/forms.py
@@ -5,11 +5,13 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class RegisterForm(UserCreationForm):
     """Sign Up Form"""
-    email = forms.EmailField(required=True, widget=forms.EmailInput())
-    username = forms.CharField(required=True)
-    birth_date = forms.DateField(widget=forms.DateInput())
-    password1 = forms.CharField(required=True, widget=forms.PasswordInput())
-    password2 = forms.CharField(required=True, widget=forms.PasswordInput())
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    first_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'First name'}))
+    last_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
+    birth_date = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
+    password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}))
 
     class Meta:
         model = User
@@ -19,5 +21,5 @@ class RegisterForm(UserCreationForm):
 class LoginUserForm(AuthenticationForm):
     """Login Form"""
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    username = forms.CharField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Username'}))
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
     password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
