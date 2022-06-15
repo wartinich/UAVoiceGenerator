0e5c132b54630a0dbad8e22767f52f8b829254e7
wartinich <vasilenko.arhip@gmail.com>
2022-06-15 13:59:11 +0300

    Add changes to logintest

--- src/users/tests.py
+++ src/users/tests.py
@@ -6,10 +6,11 @@ class LogInTest(TestCase):
     def setUp(self):
         self.credentials = {
             'email': 'laqpzd@gmail.com',
-            'username': 'admin',
+            'username': 'test',
             'password': 'secret'
         }
         User.objects.create_user(**self.credentials)
+
     def test_login(self):
         response = self.client.post('/login/', self.credentials, follow=True)
         self.assertTrue(response.status_code == 200)
