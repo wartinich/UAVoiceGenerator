927781f029542922510c4077206007a390f5fa4a
wartinich <vasilenko.arhip@gmail.com>
2022-06-15 15:49:35 +0300

    Add test to signup and login

--- src/users/tests.py
+++ src/users/tests.py
@@ -2,15 +2,38 @@ from users.models import User
 from django.test import TestCase
 
 
+class SignUpTest(TestCase):
+    def setUp(self) -> None:
+        self.email = 'kolpk@gmail.com'
+        self.username = 'kolpk'
+        self.password = 'secret'
+
+    def test_signup_url(self):
+        response = self.client.get('/sign_up/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_form(self):
+        response = self.client.post('/sign_up/', data={
+            'username': self.username,
+            'email': self.email,
+            'password1': self.password,
+            'password2': self.password
+        })
+        self.assertEqual(response.status_code, 200)
+
+
 class LogInTest(TestCase):
     def setUp(self):
         self.credentials = {
-            'email': 'laqpzd@gmail.com',
             'username': 'test',
             'password': 'secret'
         }
         User.objects.create_user(**self.credentials)
 
-    def test_login(self):
+    def test_login_url(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_form(self):
         response = self.client.post('/login/', self.credentials, follow=True)
-        self.assertTrue(response.status_code == 200)
+        self.assertEqual(response.status_code, 200)
