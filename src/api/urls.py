7d3c201349bc989e145b6db2f5ca4343adc3fa20
wartinich <vasilenko.arhip@gmail.com>
2022-07-13 10:00:43 +0300

    Setup swagger schema

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,25 +1,37 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from djoser.views import UserViewSet
-from api.views import RecordHistoryView, VoiceGeneratorView
+from api.views import RecordViewSet
+from drf_yasg.utils import swagger_auto_schema
+
+
+docs_user = \
+    swagger_auto_schema(
+        tags=['user']
+    )(UserViewSet.as_view({'get': 'me'}))
 
 
 urlpatterns = [
+    # Auth
     path('sign_up/', UserViewSet.as_view({'post': 'create'})),
 
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
-    path('user/me/', UserViewSet.as_view({'get': 'me'})),
+    # Profile
+    path('user/me/', docs_user),
     path('user/me/update/', UserViewSet.as_view({'patch': 'me', 'put': 'me'})),
 
+    # Password reset
     path('password_reset/', UserViewSet.as_view({'post': 'reset_password'})),
     path('password_reset/confirm/', UserViewSet.as_view({'post': 'reset_password_confirm'})),
 
+    # Password change
     path('change_password/', UserViewSet.as_view({'post': 'set_password'})),
 
-    path('record_history/', RecordHistoryView.as_view(), name='record_history'),
-    path('generate/', VoiceGeneratorView.as_view())
+    # Records
+    path('record_history/', RecordViewSet.as_view({'get': 'list'}), name='record_history'),
+    path('generate/', RecordViewSet.as_view({'post': 'create'}))
 
 ]
